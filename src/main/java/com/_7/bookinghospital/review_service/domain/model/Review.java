6406b77a4a50e7309b8a27a77451dfa5aa2b60f2
808dominico@naver.com <808dominico@naver.com>
2025-04-08 23:24:42 +0900

    feature: Review Entity 생성

--- src/main/java/com/_7/bookinghospital/review_service/domain/model/Review.java
+++ src/main/java/com/_7/bookinghospital/review_service/domain/model/Review.java
@@ -1,4 +1,39 @@
 package com._7.bookinghospital.review_service.domain.model;
 
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "p_review")
 public class Review {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+
+	@Column(nullable = false)
+	private Long userId;
+
+	@Column(nullable = false)
+	private UUID reservationId;
+
+	@Column(nullable = false)
+	private String title;
+
+	@Column(nullable = false)
+	private String content;
+
+	@Column(nullable = false)
+	private Integer rating;
 }
